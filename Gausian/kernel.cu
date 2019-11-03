fc2ce5b30b77ceb0e197bf8bfe83bcc2204765d4
Elias Al Homsi <elias.alhomsi@mail.mcgill.ca>
2019-11-03 16:53:24 -0500

    fix compile error

--- Gausian/kernel.cu
+++ Gausian/kernel.cu
@@ -3,11 +3,173 @@
 #include "device_launch_parameters.h"
 #include "A.h"
 #include "B.h"
-#include "rational.h"
 
 #include <string.h>
 #include <stdio.h>
 
+#include "rational.h"
+#include <math.h>
+#include <float.h>
+#include <stdio.h>
+
+
+
+__device__ long gcd(long a, long b)
+{
+	long temp;
+
+	while (b != 0)
+	{
+		temp = a % b;
+
+		a = b;
+		b = temp;
+	}
+
+	return a;
+}
+
+
+ long gcd2(long a, long b)
+{
+	long temp;
+
+	while (b != 0)
+	{
+		temp = a % b;
+
+		a = b;
+		b = temp;
+	}
+
+	return a;
+}
+
+ double dabs(double x)
+{
+	return x > 0 ? x : -x;
+}
+
+ __device__ rational_t rational_scale(rational_t r, int scale, int inverse)
+ {
+	 if (inverse)
+	 {
+		 return  {
+			 r.numerator / scale,
+			 r.denominator / scale
+		 };
+	 }
+
+	 return {
+		 r.numerator * scale,
+		 r.denominator * scale
+	 };
+ }
+
+  rational_t rational_scale2(rational_t r, int scale, int inverse)
+ {
+	 if (inverse)
+	 {
+		 return  {
+			 r.numerator / scale,
+			 r.denominator / scale
+		 };
+	 }
+
+	 return {
+		 r.numerator * scale,
+		 r.denominator * scale
+	 };
+ }
+
+rational_t rational_reduced_form2(long numerator, long denominator)
+{
+	long scale = gcd2(numerator, denominator);
+	rational_t r = {
+		numerator,
+		denominator
+	};
+
+	return rational_scale2(r, scale, 1);
+}
+
+
+__device__ rational_t rational_reduced_form(long numerator, long denominator)
+{
+	long scale = gcd(numerator, denominator);
+	rational_t r = {
+		numerator,
+		denominator
+	};
+
+	return rational_scale(r, scale, 1);
+}
+
+rational_t rational_init(double x)
+{
+	long variable = 1;
+	while (dabs(round(x) - x) > 2.2204460492503131e-016)
+	{
+		x *= 10;
+		variable *= 10;
+	}
+
+	return rational_reduced_form2(
+		(long)round(x),
+		variable);
+}
+
+__device__ rational_t rational_negate(rational_t x)
+{
+	return {
+		-x.numerator,
+			x.denominator
+	};
+}
+
+__device__ rational_t rational_add(rational_t x, rational_t y)
+{
+	if (y.denominator == x.denominator)
+	{
+		return rational_reduced_form(x.numerator + y.numerator, x.denominator);
+	}
+
+	rational_t xScaled = rational_scale(x, y.denominator, 0);
+	rational_t yScaled = rational_scale(y, x.denominator, 0);
+
+	return rational_reduced_form(
+		xScaled.numerator + yScaled.numerator,
+		xScaled.denominator);
+}
+
+__device__ rational_t rational_subtract(rational_t x, rational_t y)
+{
+	return rational_add(x, rational_negate(y));
+}
+
+__device__ rational_t rational_multiply(rational_t x, rational_t y)
+{
+	return rational_reduced_form(
+		x.numerator * y.numerator,
+		x.denominator * y.denominator);
+}
+
+__device__ rational_t rational_divide(rational_t x, rational_t y)
+{
+	// Assume denominator is always nonzero
+	if (y.numerator == 0)
+	{
+		return RATIONAL_NAN;
+	}
+
+	return rational_reduced_form(
+		x.numerator * y.denominator,
+		x.denominator * y.numerator);
+}
+
+ double get_value(rational_t x) {
+	return (1.0 * x.numerator) / x.denominator;
+}
 
 __device__ void printMatrix(double* matrix, double* b, unsigned dimension)
 {
@@ -125,7 +287,7 @@ __global__ void gaussianEliminationKernel(rational_t* matrix, unsigned dimension
 
 int main()
 {
-	const unsigned matrixSize = (sizeof(A) / sizeof(double));
+	const unsigned matrixSize = (unsigned) sqrt(sizeof(A) / sizeof(double));
 
 	rational_t* a = new rational_t[matrixSize * matrixSize];
 	// convert double A to rational_t a
--- Gausian/rational.h
+++ Gausian/rational.h
@@ -8,12 +8,12 @@ typedef struct {
 
 #define RATIONAL_NAN { 0, 0 };
 
-__device__ rational_t rational_init(double x);
+ rational_t rational_init(double x);
 __device__ rational_t rational_negate(rational_t x);
 __device__ rational_t rational_add(rational_t x, rational_t y);
 __device__ rational_t rational_subtract(rational_t x, rational_t y);
 __device__ rational_t rational_multiply(rational_t x, rational_t y);
 __device__ rational_t rational_divide(rational_t x, rational_t y);
-__device__ double get_value(rational_t x);
+ double get_value(rational_t x);
 
 #endif
